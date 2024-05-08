a6efe5ea2d0ff03773811530551ff2101707f924
Pooja Yadav <168546801+pooja7798@users.noreply.github.com>
2024-05-08 19:39:54 +0530

    Create task3.js

---
+++ HTML_CSS_JS/Day 2 /task3.js
@@ -0,0 +1,24 @@
+
+//write a function to add any number of integers (both ES5 and ES6)
+
+//es5 function
+function addInteger(){
+    sum = 0;
+    for(var i = 0; i < arguments.length; i++){
+        if(typeof(arguments[i]) === "number"){
+            sum += arguments[i];
+        }
+    }
+    return sum;
+}
+
+let inputCheck = addInteger(10,30,40);
+console.log(inputCheck);
+
+//es6 function
+function addIntegers(...numbers) {
+    return numbers.reduce((sum, num) => sum + num, 0);
+  }
+
+let inputCheck2 = addIntegers(5,20,34,66);
+console.log(inputCheck2);
